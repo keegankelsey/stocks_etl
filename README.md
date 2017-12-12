27902d64a4fe2a7478e6c6ec04e7346ef1175e44
Keegan Kelsey <keegankelsey@users.noreply.github.com>
2017-12-11 22:01:19 -0800

    New script to process API data

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # utils_stocks
-A utilities repo for extracting and processing stock data.
+Purpose: For extracting and processing stock data from the free service, Alpha Vantage ([www.alphavantage.co](https://www.alphavantage.co/)).
 
 -----------
 -----------
 
 ## 1. src/api_alphavantage_extract.py
 
-This is a python script that uses the Alpha Vantage API. Documentation and API key (required) may be obtained at [www.alphavantage.co](https://www.alphavantage.co/).
+This is a script that uses the Alpha Vantage API. Documentation for underlying source data and obtaining an API key (required) may be found at [www.alphavantage.co](https://www.alphavantage.co/).
 
-The script currently supports two functions supplied by Alpha Vantage; a function that 1) returns intraday stock quotes and a function that 2) returns end-of-day (EOD) stock quotes.
+The script currently supports two functions supplied by Alpha Vantage: 1) a function that returns intraday stock quotes and, 2) a function that returns end-of-day (EOD) summary data.
 
-`data/symbols.txt` is supplied and contains four stock symbols (AAPL, AMZN, GOOG and TWTR) for the purpose of testing.
+`symbols.txt` is located in the data directory and contains four stock symbols (AAPL, AMZN, GOOG and TWTR) for the purpose of testing.
 
 
 ***Example 1:*** Extract the last 100 time points from stocks, based on 5 minute intervals.
@@ -24,3 +24,23 @@ python api_alphavantage_extract.py time_series_daily_adjusted full ../data/symbo
 ```
 
 -----------
+
+## 2. src/api_alphavantage_transform.py
+
+This is a script that extracts desired dates from api_alphavantage_extract.py output. Two functions (intraday and daily) are supported. This script also restructures the data to supply one JSON object per stock symbol and time period.
+
+`quotes_intraday.json` and `quotes_daily.json` are located in the data directory and are supplied for the purpose of testing.
+
+
+***Example 1:*** Extract intraday stock quotes from 2017-12-11
+```
+python api_alphavantage_transform.py ../data/quotes_intraday.json -sd 2017-12-11 -ed 2017-12-11
+```
+
+***Example 2:*** Extract EOD summary data for dates ranging from 2017-12-01 to 2017-12-05.
+```
+python api_alphavantage_transform.py ../data/quotes_daily.json -sd 2017-12-01 -ed 2017-12-05
+```
+
+-----------
+
---
+++ src/api_alphavantage_transform.py
@@ -0,0 +1,85 @@
+from sys import argv
+import sys
+import json
+import argparse
+from datetime import datetime, timedelta
+from pytz import timezone
+import pytz
+
+
+# Specify default start and end dates using Eastern TZ
+utc = pytz.utc
+eastern = timezone('US/Eastern')
+now = datetime.now(tz=utc)
+now_date = now.astimezone(eastern).strftime('%Y-%m-%d')
+
+# Define and parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('stock_quotes',
+    type=str,
+    help='Output from api_alphavantage.py')
+parser.add_argument('-sd', '--start_date',
+    type = str,
+    default = now_date,
+    help = 'Desired start date. For example, "2017-12-07." Default is current date (Eastern time zone).')
+parser.add_argument('-ed', '--end_date',
+    type = str,
+    default = now_date,
+    help = 'Desired end date. For example, "2017-12-11." Default is current date (Eastern time zone).')
+args = parser.parse_args()
+
+# Raise error if start_date is after end_date
+if args.start_date > args.end_date:
+    print('start_date, {}, must be earlier than or equal to end_date, {}.'.format(args.start_date, args.end_date))
+    sys.exit('Bye')
+
+# Begin functions to extract and print stock symbol data
+def read_stock_quotes(stock_quotes, start_date, end_date):
+    """ Convert output from api call to desired set
+        of quote dates """
+    with open(args.stock_quotes, 'r') as f:
+        for line in f:
+            x = json.loads(line)
+            symbol = x['Meta Data']['2. Symbol']
+            quote_key = list(x)[1]
+            if quote_key == 'Time Series (Daily)':
+                for date_time in list(x[quote_key]):
+                    if start_date <= date_time <= end_date:
+                        d = x[quote_key][date_time]
+                        obj = {'symbol': symbol,
+                            'trade_date': date_time,
+                            'open': float(d['1. open']),
+                            'high': float(d['2. high']),
+                            'low': float(d['3. low']),
+                            'close': float(d['4. close']),
+                            'adjusted_close': float(d['5. adjusted close']),
+                            'volume': float(d['6. volume']),
+                            'divident_amount': float(d['7. dividend amount']),
+                            'split_coefficient': float(d['8. split coefficient'])}
+                        print(json.dumps(obj))
+            elif quote_key == 'Time Series (1min)' or \
+                quote_key == 'Time Series (5min)' or \
+                quote_key == 'Time Series (15min)' or \
+                quote_key == 'Time Series (30min)' or \
+                quote_key == 'Time Series (60min)':
+                for date_time in list(x[quote_key]):
+                    tm = date_time.split(' ')
+                    trade_date = tm[0]
+                    trade_time = tm[1]
+                    if start_date <= trade_date <= end_date:
+                        d = x[quote_key][date_time]
+                        obj = {'symbol': symbol,
+                            'trade_date': trade_date,
+                            'trade_time': trade_time,
+                            'open': float(d['1. open']),
+                            'high': float(d['2. high']),
+                            'low': float(d['3. low']),
+                            'close': float(d['4. close']),
+                            'volume': float(d['5. volume'])}
+                        print(json.dumps(obj))
+            else:
+                print('Data type is not supported')
+                sys.exit('Bye')
+
+# Run functions
+read_stock_quotes(args.stock_quotes, args.start_date, args.end_date)
